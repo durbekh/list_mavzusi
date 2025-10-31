03597777dccc6129f2c31029b6268853dbc3bfc0
Durbek Hayotbekov <d83217311@gmail.com>
2025-10-31 16:16:53 +0500

    Add files via upload

---
+++ list_masalalar.py
@@ -0,0 +1,160 @@
+# # 1
+# fruits = ['olma', 'banan', 'uzum']
+# fruits.append('nok')
+# print(fruits)
+# print(len(fruits))
+# print("olma" in fruits)
+# print(fruits.index("banan"))
+# # 2
+# colors = ["qizil", "yashil", "ko'k"]
+# colors.append("sariq")
+# print(colors)
+# colors.sort()
+# print(colors)
+# print(len(colors))
+# print("yashil" in colors)
+# # 3
+# cities = ['Toshkent', 'Samarqand', 'Buxoro']
+# cities.append('Xiva')
+# print(cities)
+# print(cities.index('Samarqand'))
+# print(len(cities))
+# cities.sort()
+# print(cities)
+# # 4
+# numbers = [5, 2, 8, 1]
+# numbers.append(10)
+# print(numbers)
+# numbers.sort()
+# print(numbers)
+# print(len(numbers))
+# print(8 in numbers)
+# # 5
+# animals = ['mushuk', 'it', 'qush']
+# animals.append('baliq')
+# print(animals)
+# animals.sort()
+# print(animals)
+# print(animals.index('it'))
+# print(len(animals))
+# # 6
+# fruits = ['olma', 'banan', 'uzum', 'nok']
+# fruits.append("kivi")
+# print(fruits)
+# fruits.remove("banan")
+# print(fruits)
+# fruits.insert(-2, "shaftoli" )
+# print(fruits)
+# fruits.sort()
+# print(fruits)
+# print(len(fruits))
+# print(fruits.index("olma"))
+# print("kivi" in fruits)
+# fruits.sort(reverse=True)
+# print(fruits)
+# fruits2 = fruits.copy()
+# fruits2.append("ananas")
+# print(fruits)
+# print(fruits2)
+# # 7
+# menu = ['osh', "lag'mon", 'shashlik', 'somsa']
+# menu.extend(["manti" , "norin"])
+# print(menu)
+# menu.remove("lag'mon")
+# print(menu)
+# menu.pop()
+# print(menu)
+# print(len(menu))
+# print(menu.index("osh"))
+# print(menu.count("somsa"))
+# print("manti" in menu)
+# meni1 = menu.copy()
+# meni1.append("qozon kabob")
+# print(menu)
+# print(meni1)
+# # 8
+# cities = ['Toshkent', 'Samarqand', 'Buxoro', 'Xiva']
+# cities.insert(0, 'Andijon')
+# print(cities)
+# cities.remove('Xiva')
+# print(cities)
+# print(cities.index('Samarqand'))
+# cities.sort()
+# print(cities)
+# print(len(cities))
+# print("Buxoro" in cities)
+# cities.pop()
+# print(cities)
+# cities.sort(reverse=True)
+# print(cities)
+# cities1 = cities.copy()
+# cities1.append("Farg'ona")
+# print(cities)
+# print(cities1)
+# # 9
+# students = ['Ali', 'Vali', 'Sardor', 'Aziz']
+# students.append("Jamshid")
+# print(students)
+# students.remove("Sardor")
+# print(students)
+# students.sort()
+# print(students)
+# print(students.index("Aziz"))
+# print("Diyor" in students)
+# students.pop()
+# print(len(students))
+# students1 = students.copy()
+# students1.append("Nodir")
+# print(students)
+# print(students1)
+# # 10
+# favorites = ['telefon', 'noutbuk', 'naushnik']
+# favorites.append("planshet")
+# print(favorites)
+# favorites.append("soat")
+# print(favorites)
+# favorites.sort()
+# print(favorites)
+# print(len(favorites))
+# print(favorites.index('naushnik'))
+# print("telefon" in favorites)
+# favorites.remove('naushnik')
+# print(favorites)
+# favorites.pop()
+# print(favorites)
+# favorites2 = favorites.copy()
+# favorites2.append("televizor")
+# favorites2.sort(reverse=True)
+# print(favorites)
+# print(favorites2)
+# # 11
+# songs = ["Yomg'ir", 'Sevgi', 'Hayot', 'Kuz']
+# songs.insert(1 , "Baxor")
+# print(songs)
+# songs.remove("Kuz")
+# print(songs)
+# songs.sort()
+# print(songs)
+# print(songs.index("Yomg'ir"))
+# print("Hayot" in songs)
+# songs.pop()
+# print(songs)
+# print(len(songs))
+# songs.sort(reverse=True)
+# print(songs)
+# songs1 = songs.copy()
+# songs1.append("Yoz")
+# print(songs)
+# print(songs1)
+
+
+
+
+
+
+
+
+
+
+
+
